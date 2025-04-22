3dafc0bd02150d3669cbcae4d92818260848d6be
hellogif98 <fathedin@gmail.com>
2025-04-22 18:22:14 +0300

    Add files via upload

---
+++ CLASS STUDENT.py
@@ -0,0 +1,6 @@
+class student: 
+    you = 50
+    years = "years old"
+    print("hello guys i am " ,you ,years)
+ob=student()
+
---
+++ actvity.py
@@ -0,0 +1,6 @@
+class student :
+    grade = 999
+    name = idk
+    def introduction(self):
+        print("hi im ")
+    
