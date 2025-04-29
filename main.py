1866f7e6a48fe99806a8452a2f3c72e60f4cdcd3
hellogif98 <fathedin@gmail.com>
2025-04-29 16:58:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+class robot:
+    you = "Tom"
+    years = "I am 10 years old i like to do dangerous tasks because im a clever cat!"
+    print("hello my name is" ,you ,years)
+ob=robot()
+
+class robot:
+    you = "Jerry"
+    years = "I am 8 years old i like to do work that my owners tell me to do."
+    print("hello my name is" ,you ,years)
+ob=robot()
+
+
