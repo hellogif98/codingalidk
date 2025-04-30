048d752ffc4506026ef67be026a5a62d96d81005
hellogif98 <fathedin@gmail.com>
2025-04-30 19:24:11 +0300

    Add files via upload

---
+++ class.py
@@ -0,0 +1,20 @@
+class cat:
+    def _init_(self, name, age ):
+        self.name = "bob"
+        self.age = 2
+
+def init(self) :
+    print(f"Hi i am a cat. my name is {self.name}. i am {self.age} years old. :) ")
+
+def make_sound(self) :
+    print("Meow")
+
+
+    class dog:
+        def _init_(self, name, age) :
+            self.name = "toiler"
+            self.age = 4
+
+def init(self) :
+    print(f"hi i am a goat. my name is {self.name}. i am {self.age} years old. :)")
+    
