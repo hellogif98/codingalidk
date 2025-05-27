024081630bd3dad80636d72cf8a1592b21249e89
hellogif98 <fathedin@gmail.com>
2025-05-27 18:08:24 +0300

    Add files via upload

---
+++ asmptotations.py
@@ -0,0 +1,11 @@
+# Program  to show Constant Time Complexity
+
+def printnumber(n):
+  iteration=0
+  print("The number entered by the user is",n)
+  iteration+=1
+  print("Total interations done by the code is",iteration,"/n")
+
+printnumber(10)
+printnumber(20)
+print("/n With any'n' the time taken by our cose won't change")
